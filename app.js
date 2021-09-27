5370d12eaf6900c5de731b920686bf1fc6a10d03
TunirC <vincenthavoc@gmail.com>
2021-09-27 21:16:11 +0530

    Testing VanillaJS Scripts

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
-var button_primary = document.querySelector("")
+alert("Sample")
 
+var button_primary = document.querySelector("#btn-click");
+var inputText = document.querySelector("#input-Text");
+var outputText = document.querySelector("#output-Text");
+
+function onClickEvent(inputText){
+    console.log(inputText)
+}
+
+addEventListener("click",function onClickEvent(inputText));
 
 // txt-area
 // .button-primary
