a56a4ea7fc26972462625a5c574823f12bd470db
TunirC <vincenthavoc@gmail.com>
2021-09-28 00:53:41 +0530

    Created Minions language translator

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
-alert("Sample")
-
-var button_primary = document.querySelector("#btn-click");
+var btnClick = document.querySelector("#btn-click");
 var inputText = document.querySelector("#input-Text");
 var outputText = document.querySelector("#output-Text");
 
-function onClickEvent(inputText){
-    console.log(inputText)
+var urlName = "	https://api.funtranslations.com/translate/minion.json";
+
+function onClickEvent(){
+    var responseURL = urlName + "?" + "text=" + inputText.value;
+
+    fetch(responseURL)
+    .then(Response => Response.json())
+    .then(json => outputText.innerText = json.contents.translated)
+
 }
 
-addEventListener("click",function onClickEvent(inputText));
 
-// txt-area
-// .button-primary
-// #input-btn
+btnClick.addEventListener("click", onClickEvent)
+
+
+// fetch("https://api.funtranslations.com/translate/minion.json?text=I am inevitable")
+// .then(Response => Response.json())
+// .then(json => console.log(json))
+
+
+
+// document.querySelector("textarea")
+// document.querySelector(".btn-primary")
+// document.querySelector("#input-btn")
+// document.querySelector("input[name='translator']")
